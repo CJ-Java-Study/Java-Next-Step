1c1794fad2efbf9680e591d753be531131e5c0c5
Minji Kang <nitronium@ewhain.net>
2025-04-20 01:40:45 +0900

    feat: calculator 및 테스트 추가
    - JUnit 5 사용

---
+++ nextstep/src/test/java/com/nextstep/calculator/CalculatorTest.java
@@ -0,0 +1,53 @@
+package com.nextstep.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("[문자열계산기] 테스트")
+public class CalculatorTest {
+
+    private Calculator cal;
+
+    @BeforeEach
+    public void setUp() {
+        cal = new Calculator();
+        System.out.println("before");
+    }
+
+    @AfterEach
+    public void teardown() {
+        System.out.println("teardown");
+    }
+
+    @Test
+    @DisplayName("덧셈_성공")
+    public void add_success() {
+        assertEquals(9, cal.add(6, 3));
+        System.out.println("add");
+    }
+
+    @Test
+    @DisplayName("뺄셈_성공")
+    public void subtract_success() {
+        assertEquals(3, cal.subtract(6, 3));
+        System.out.println("subtract");
+    }
+
+    @Test
+    @DisplayName("곱셈_성공")
+    public void multiply_success() {
+        assertEquals(18, cal.multiply(6, 3));
+        System.out.println("multiply");
+    }
+
+    @Test
+    @DisplayName("나눗셈_성공")
+    public void divide_success() {
+        assertEquals(2, cal.divide(6, 3));
+        System.out.println("divide");
+    }
+}
