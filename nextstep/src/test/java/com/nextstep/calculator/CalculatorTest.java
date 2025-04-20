23f23773427bbde8155c1596a300e8d7a54f4e10
Minji Kang <nitronium@ewhain.net>
2025-04-21 00:20:53 +0900

    test(calculator): test에서 public 제거

--- nextstep/src/test/java/com/nextstep/calculator/CalculatorTest.java
+++ nextstep/src/test/java/com/nextstep/calculator/CalculatorTest.java
@@ -7,46 +7,45 @@ import org.junit.jupiter.api.DisplayName;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@DisplayName("[문자열계산기] 테스트")
-public class CalculatorTest {
-
+@DisplayName("[계산기]")
+class CalculatorTest {
     private Calculator cal;
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         cal = new Calculator();
         System.out.println("before");
     }
 
     @AfterEach
-    public void teardown() {
+    void teardown() {
         System.out.println("teardown");
     }
 
     @Test
     @DisplayName("덧셈_성공")
-    public void add_success() {
+    void add_success() {
         assertEquals(9, cal.add(6, 3));
         System.out.println("add");
     }
 
     @Test
     @DisplayName("뺄셈_성공")
-    public void subtract_success() {
+    void subtract_success() {
         assertEquals(3, cal.subtract(6, 3));
         System.out.println("subtract");
     }
 
     @Test
     @DisplayName("곱셈_성공")
-    public void multiply_success() {
+    void multiply_success() {
         assertEquals(18, cal.multiply(6, 3));
         System.out.println("multiply");
     }
 
     @Test
     @DisplayName("나눗셈_성공")
-    public void divide_success() {
+    void divide_success() {
         assertEquals(2, cal.divide(6, 3));
         System.out.println("divide");
     }
