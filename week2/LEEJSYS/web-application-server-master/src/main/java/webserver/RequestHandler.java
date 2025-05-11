80c07267ece4c6e58cdd276a85c3a040148f5990
LeeJSYS <dauzzang@gmail.com>
2025-05-11 21:11:43 +0900

    feat: 요구사항5,6 구현 - 단, 회원가입계정 저장 안되어 로그인 검증 로직은 추후 추가 예정

--- week2/LEEJSYS/web-application-server-master/src/main/java/webserver/RequestHandler.java
+++ week2/LEEJSYS/web-application-server-master/src/main/java/webserver/RequestHandler.java
@@ -3,9 +3,11 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import db.DataBase;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -56,9 +58,18 @@ public class RequestHandler extends Thread {
 
             if (url.startsWith("/user/create") && method.equals("GET")) {
                 handleUserCreate(url, dos);
-            }if (url.equals("/user/create") && method.equals("POST")) {
+            }else if (url.equals("/user/create") && method.equals("POST")) {
                 handleUserCreatePost(br, headers.get("Content-Length"), dos);
-            } else {
+            }else if (url.equals("/user/login") && method.equals("POST")) {
+                String contentLengthHeader = headers.get("Content-Length");
+                if (contentLengthHeader != null) {
+                    handleUserLoginPost(br, contentLengthHeader, dos);
+                } else {
+                    response400(dos, "Missing Content-Length header");
+                }
+            }else if (url.equals("/user/list") && method.equals("GET")) {
+                handleUserList(dos);
+            }else {
                 serveIndexFile(url, dos);
             }
 
@@ -83,7 +94,7 @@ public class RequestHandler extends Thread {
                 params.get("name"),
                 params.get("email")
         );
-
+        DataBase.addUser(user);
         log.debug("회원가입 요청 처리: {}", user);
 
         byte[] body = "회원가입 완료".getBytes();
@@ -106,7 +117,7 @@ public class RequestHandler extends Thread {
         log.debug("POST 회원가입 요청 처리: {}", user);
 
         byte[] responseBody = "회원가입 완료 (POST)".getBytes();
-        response200(dos, responseBody);
+        response302(dos, "/index.html", null);
     }
 
     private void serveIndexFile(String url, DataOutputStream dos) throws IOException {
@@ -124,6 +135,47 @@ public class RequestHandler extends Thread {
         }
     }
 
+    private void handleUserLoginPost(BufferedReader br, String contentLengthHeader, DataOutputStream dos) throws IOException {
+        int contentLength = Integer.parseInt(contentLengthHeader);
+        String body = IOUtils.readData(br, contentLength);
+        Map<String, String> params = HttpRequestUtils.parseQueryString(body);
+
+        String userId = params.get("userId");
+        String password = params.get("password");
+
+        log.debug("userId 저장 확인: {}", userId);
+        User user = DataBase.findUserById(userId);
+
+        log.debug("로그인 요청 내용: {}", user);
+
+        if (user != null && user.getPassword().equals(password)) {
+            response302(dos, "/index.html", "logined=true");
+        } else {
+            response302(dos, "/user/login_failed.html", "logined=false");
+        }
+    }
+
+    private void handleUserList(DataOutputStream dos) throws IOException {
+        Collection<User> users = DataBase.findAll();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<html><body>");
+        sb.append("<h1>사용자 목록</h1>");
+        sb.append("<ul>");
+        for (User user : users) {
+            sb.append("<li>")
+                    .append(user.getUserId())
+                    .append(" - ")
+                    .append(user.getName())
+                    .append("</li>");
+        }
+        sb.append("</ul>");
+        sb.append("</body></html>");
+
+        byte[] body = sb.toString().getBytes();
+        response200(dos, body);
+    }
+
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
@@ -133,6 +185,24 @@ public class RequestHandler extends Thread {
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private void response302Header(DataOutputStream dos, String location, String cookie, int lengthOfBodyContent) throws IOException {
+        dos.writeBytes("HTTP/1.1 302 Found\r\n");
+        dos.writeBytes("Location: " + location + "\r\n");
+        if (cookie != null && !cookie.isEmpty()) {
+            dos.writeBytes("Set-Cookie: " + cookie + "\r\n");
+        }
+        dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+        dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+        dos.writeBytes("\r\n");
+    }
+
+    private void response400Header(DataOutputStream dos, int lengthOfBodyContent) throws IOException {
+        dos.writeBytes("HTTP/1.1 400 Bad Request\r\n");
+        dos.writeBytes("Content-Type: text/plain;charset=utf-8\r\n");
+        dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+        dos.writeBytes("\r\n");
     }
 
     private void response404Header(DataOutputStream dos, int lengthOfBodyContent) throws IOException {
@@ -153,6 +223,18 @@ public class RequestHandler extends Thread {
 
     private void response200(DataOutputStream dos, byte[] body) throws IOException {
         response200Header(dos, body.length);
+        responseBody(dos, body);
+    }
+
+    private void response302(DataOutputStream dos, String location, String cookie) throws IOException {
+        byte[] body = "<h1>Redirecting...</h1>".getBytes();
+        response302Header(dos, location, cookie, body.length);
+        responseBody(dos, body);
+    }
+
+    private void response400(DataOutputStream dos, String message) throws IOException {
+        byte[] body = message.getBytes();
+        response400Header(dos, body.length);
         responseBody(dos, body);
     }
 
