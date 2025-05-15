33ca6a950effd3108c11caa556bd8a09a2e47ea3
Minji Kang <nitronium@ewhain.net>
2025-05-16 00:38:19 +0900

    refactor: requestHandler의 controller 관련 로직 추출

--- week3/nitronium102/src/main/java/webserver/RequestHandler.java
+++ week3/nitronium102/src/main/java/webserver/RequestHandler.java
@@ -24,44 +24,16 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            HttpRequest httpRequest = new HttpRequest(in);
+            HttpRequest request = new HttpRequest(in);
             HttpResponse response = new HttpResponse(out);
-            String path = httpRequest.getPath();
 
+            String path = getDefaultPath(request.getPath());
             if ("/user/create".equals(path)) {
-                Map<String, String> params = httpRequest.getParameterMap();
-                User user = new User(
-                        params.get("userId"),
-                        params.get("password"),
-                        params.get("name"),
-                        params.get("email")
-                );
-                log.debug("user : {}", user);
-                DataBase.addUser(user);
-                response.sendRedirect("/index.html");
+                createUser(request, response);
             } else if ("/user/login".equals(path)) {
-                Map<String, String> params = httpRequest.getParameterMap();
-                User user = DataBase.findUserById(params.get("userId"));
-                if (user != null && user.getPassword().equals(params.get("password"))) {
-                    response.addHeader("Set-Cookie", "logined=true");
-                    response.sendRedirect("/index.html");
-                } else {
-                    response.sendRedirect("/user/login_failed.html");
-                }
+                loginUser(request, response);
             } else if ("/user/list".equals(path)) {
-                if (httpRequest.isLoggedIn()) {
-                    response.sendRedirect("/user/login.html");
-                    return;
-                }
-                StringBuilder sb = new StringBuilder();
-                for (User user : DataBase.findAll()) {
-                    sb.append("<tr>");
-                    sb.append("<td>" + user.getUserId() + "</td>");
-                    sb.append("<td>" + user.getName() + "</td>");
-                    sb.append("<td>" + user.getEmail() + "</td>");
-                    sb.append("<tr>");
-                }
-                response.forwardBody(sb.toString());
+                listUser(request, response);
             } else {
                 response.forward(path);
             }
@@ -69,5 +41,58 @@ public class RequestHandler extends Thread {
                 IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private void createUser(HttpRequest request, HttpResponse response) {
+        Map<String, String> params = request.getParameterMap();
+        User user = new User(
+                params.get("userId"),
+                params.get("password"),
+                params.get("name"),
+                params.get("email")
+        );
+        log.debug("user : {}", user);
+        DataBase.addUser(user);
+        response.sendRedirect("/index.html");
+    }
+
+    private void loginUser(HttpRequest request, HttpResponse response) {
+        Map<String, String> params = request.getParameterMap();
+        User user = DataBase.findUserById(params.get("userId"));
+        if (user != null && user.getPassword().equals(params.get("password"))) {
+            response.addHeader("Set-Cookie", "logined=true");
+            response.sendRedirect("/index.html");
+        } else {
+            response.sendRedirect("/user/login_failed.html");
+        }
+    }
+
+    private void listUser(HttpRequest request, HttpResponse response) {
+        if (request.isLoggedIn()) {
+            response.sendRedirect("/user/login.html");
+            return;
+        }
+        response.forwardBody(generateUserListHtml());
+    }
+
+    private String generateUserListHtml() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<table class='table'>");
+        DataBase.findAll().forEach(user -> {
+            sb.append("<tr>")
+                    .append(String.format("<td>%s</td>", user.getUserId()))
+                    .append(String.format("<td>%s</td>", user.getName()))
+                    .append(String.format("<td>%s</td>", user.getEmail()))
+                    .append("</tr>");
+        });
+        sb.append("</table>");
+        return sb.toString();
+    }
+
+    private String getDefaultPath(String path) {
+        if (path.equals("/")) {
+            return "/index.html";
+        }
+        return path;
     }
 }
