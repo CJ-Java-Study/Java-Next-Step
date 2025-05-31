195d2c3b16360de7eac1b13865b8d3df1f010c53
Minji Kang <nitronium@ewhain.net>
2025-06-01 02:18:43 +0900

    6.3.4 클라이언트별 세션 저장소 추가(HttpSession)

--- web-server/src/main/java/http/HttpRequest.java
+++ web-server/src/main/java/http/HttpRequest.java
@@ -67,4 +67,8 @@ public class HttpRequest {
 	public HttpCookie getCookies() {
 		return new HttpCookie(getHeader("Cookie"));
 	}
+
+	public HttpSession getSession(){
+		return HttpSessions.getSession(getCookies().getValue("JSESSIONID"));
+	}
 }
---
+++ web-server/src/main/java/http/HttpSession.java
@@ -0,0 +1,30 @@
+package http;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpSession {
+    private Map<String, Object> values = new HashMap<>();
+
+    private String sessionId;
+
+    public HttpSession(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public void setAttribute(String key, String value) {
+        values.put(key, value);
+    }
+
+    public Object getAttribute(String key) {
+        return values.get(key);
+    }
+
+    public void removeAttribute(String key) {
+        values.remove(key);
+    }
+
+    public void invalidate() {
+        HttpSessions.removeSession(sessionId);
+    }
+}
