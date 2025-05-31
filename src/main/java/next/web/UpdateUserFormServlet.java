0332dff358aecb135a7d4cb3a16e1c9f33991b77
Minji Kang <nitronium@ewhain.net>
2025-06-01 01:42:07 +0900

    6.1.4 회원 목록 및 개인정보 수정 보안 강화

--- src/main/java/next/web/UpdateUserFormServlet.java
+++ src/main/java/next/web/UpdateUserFormServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Slf4j
@@ -23,6 +24,11 @@ public class UpdateUserFormServlet extends HttpServlet {
         String userId = req.getParameter("userId");
         User user = DataBase.findUserById(userId);
 
+        HttpSession session = req.getSession();
+        if(session.getAttribute("user") != user) {
+            throw new IllegalArgumentException("다른 사용자의 정보를 수정할 수 없습니다.");
+        }
+
         req.setAttribute("user", user);
         RequestDispatcher rd = req.getRequestDispatcher("/user/update.jsp");
         rd.forward(req, resp);
@@ -32,6 +38,11 @@ public class UpdateUserFormServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String userId = req.getParameter("userId");
         User user = DataBase.findUserById(userId);
+
+        HttpSession session = req.getSession();
+        if(session.getAttribute("user") != user) {
+            throw new IllegalArgumentException("다른 사용자의 정보를 수정할 수 없습니다.");
+        }
 
         if (user == null) {
             log.warn("User with id {} not found", userId);
