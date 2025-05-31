4e3779e099d69b909e636cf7a57f0170858601cc
Minji Kang <nitronium@ewhain.net>
2025-06-01 01:37:16 +0900

    6.2.3 로그인/로그아웃 실습

---
+++ src/main/java/next/web/LogoutUserServlet.java
@@ -0,0 +1,24 @@
+package next.web;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@Slf4j
+@WebServlet("/user/logout")
+public class LogoutUserServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        session.removeAttribute("user");
+        resp.sendRedirect("/");
+    }
+}
