b055cbde346d99343a91bfcb31bebc0e3db1a95c
cherin kim <76779953+mawakeb@users.noreply.github.com>
2025-06-14 23:20:48 +0900

    fix: HomeController WebServlet 어노테이션 제거

--- week5/mawakeb/src/main/java/next/controller/HomeController.java
+++ week5/mawakeb/src/main/java/next/controller/HomeController.java
@@ -3,14 +3,12 @@ package next.controller;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import core.db.DataBase;
 import next.controller.common.AbstractController;
 
-@WebServlet("")
 public class HomeController extends AbstractController {
     private static final long serialVersionUID = 1L;
 
