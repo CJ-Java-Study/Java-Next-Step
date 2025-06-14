66f713e9ac3314de8924cfe900c3f26220e05187
cherin kim <76779953+mawakeb@users.noreply.github.com>
2025-06-14 22:28:27 +0900

    feat: DispatcherServlet 추가
    loadOnStartup
    > when the number is a positive integer, load and initialize this servlet as soon as the application starts (in order)
    > if it is negative it is loaded lazily

---
+++ week5/mawakeb/src/main/java/next/controller/DispatcherServlet.java
@@ -0,0 +1,10 @@
+package next.controller;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(name = "dispatcher", urlPatterns = "/", loadOnStartup = 1)
+public class DispatcherServlet {
+
+
+
+}
