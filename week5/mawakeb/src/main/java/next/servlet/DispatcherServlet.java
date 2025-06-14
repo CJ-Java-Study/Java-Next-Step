9902b04e09754209dffe9e2b0850e7ddc241f200
cherin kim <76779953+mawakeb@users.noreply.github.com>
2025-06-14 23:18:39 +0900

    feat: DispatcherServlet 구현

--- week5/mawakeb/src/main/java/next/servlet/DispatcherServlet.java
+++ week5/mawakeb/src/main/java/next/servlet/DispatcherServlet.java
@@ -1,10 +1,43 @@
 package next.servlet;
 
+import next.controller.common.Controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(name = "dispatcher", urlPatterns = "/", loadOnStartup = 1)
-public class DispatcherServlet {
+public class DispatcherServlet extends HttpServlet {
 
+    private static final long serialVersionUID = 1L;
+    private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
+    private static final String REDIRECT = "redirect:";
 
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String requestUri = request.getRequestURI();
+        logger.debug("Method : {}, Request URI : {}", request.getMethod(), requestUri);
 
+        Controller controller = RequestMapping.getController(requestUri);
+        try {
+            String path = controller.execute(request, response);
+            if (path.startsWith(REDIRECT)) {
+                response.sendRedirect(path.substring(REDIRECT.length()));
+                return;
+            }
+
+            RequestDispatcher rd = request.getRequestDispatcher(path);
+            rd.forward(request, response);
+
+        } catch (Exception e) {
+            logger.error("Exception : {}", e.getMessage());
+            throw new ServletException(e.getMessage());
+        }
+    }
 }
