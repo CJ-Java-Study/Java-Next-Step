d28fa1f686005e414a6fd080508ed2be9fb62943
Minji Kang <nitronium@ewhain.net>
2025-06-29 18:30:41 +0900

    feat: createUserController를 UserDao 활용하도록 변경

--- src/main/java/next/controller/CreateUserController.java
+++ src/main/java/next/controller/CreateUserController.java
@@ -3,12 +3,14 @@ package next.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import next.dao.UserDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import core.db.DataBase;
 import core.mvc.Controller;
 import next.model.User;
+
+import java.sql.SQLException;
 
 public class CreateUserController implements Controller {
     private static final Logger log = LoggerFactory.getLogger(CreateUserController.class);
@@ -17,9 +19,18 @@ public class CreateUserController implements Controller {
     public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         User user = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"),
                 req.getParameter("email"));
-        log.debug("User : {}", user);
+        log.debug("userId={}, password={}, name={}, email={}",
+                user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
 
-        DataBase.addUser(user);
+        UserDao userDao = new UserDao();
+        try{
+            userDao.insert(user);
+            User entity = userDao.findByUserId(user.getUserId());
+            log.debug("userId-db={}, password-db={}, name-db={}, email-db={}",
+                    entity.getUserId(), entity.getPassword(), entity.getName(), entity.getEmail());
+        } catch(SQLException e){
+            log.error(e.getMessage());
+        }
         return "redirect:/";
     }
 }
